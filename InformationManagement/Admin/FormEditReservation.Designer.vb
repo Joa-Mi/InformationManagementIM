066ee344ce1d42910f29e141b76172e257b3e21a
Joa-Mi <mmik70999@gmail.com>
2025-11-05 00:54:24 +0800

    _____6

---
+++ InformationManagement/Admin/FormEditReservation.Designer.vb
@@ -0,0 +1,56 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormEditReservation
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'FormEditReservation
+        '
+        Me.ClientSize = New System.Drawing.Size(554, 469)
+        Me.Name = "FormEditReservation"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtFullName As RoundedTextBox
+    Friend WithEvents txtEmail As RoundedTextBox
+    Friend WithEvents txtPhone As RoundedTextBox
+    Friend WithEvents numGuests As NumericUpDown
+    Friend WithEvents cmbEventType As ComboBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents DateTimePicker2 As DateTimePicker
+    Friend WithEvents txtNote As RoundedTextBox
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnUpdateReservation As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents cmbStatus As ComboBox
+    Friend WithEvents Label10 As Label
+End Class
